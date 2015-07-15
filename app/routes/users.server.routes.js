4b9a255309e58c0136a059d8520bdf398ca4e041
mzpolini <matthew.polini@gmail.com>
2015-07-14 22:10:30 -0400

    ready for OAuth

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,7 +1,28 @@
-var users = require('../../app/controllers/users.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+  passport = require('passport');
 
 module.exports = function(app) {
-  app.route('/users')
-    .post(users.create)
-    .get(users.list);
+  app.route('/signup')
+    .get(users.renderSignup)
+    .post(users.signup);
+
+  app.route('/signin')
+    .get(users.renderSignin)
+    .post(passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/signin',
+      failureFlash: true
+    }));
+
+  app.get('/signout', users.signout);
+  // app.route('/users')
+  //   .post(users.create)
+  //   .get(users.list);
+  //
+  // app.route('/users/:userId')
+  //   .get(users.read)
+  //   .put(users.update)
+  //   .delete(users.delete);
+  //
+  // app.param('userId', users.userByID);
 };
